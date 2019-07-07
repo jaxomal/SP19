142600b2b8d37e69a71b4b22af136227796832ff
Willis Ng <wng11@stuy.edu>
2019-07-06 20:18:52 -0400

    Finished the Body Class and its Constructor

---
+++ proj0/Body.java
@@ -0,0 +1,27 @@
+public class Body {
+	public double xxPos;
+	public double yyPos;
+	public double xxVel;
+	public double yyVel;
+	public double mass;
+	public String imgFileName;
+
+	public Body(double xxPos, double yyPos, double xxVel,
+		double yyVel, double mass, String imgFileName) {
+		this.xxPos = xxPos;
+		this.yyPos = yyPos;
+		this.xxVel = xxVel;
+		this.yyVel = yyVel;
+		this.mass = mass;
+		this.imgFileName = imgFileName;
+	}
+
+	public Body(Body b) {
+		this.xxPos = b.xxPos;
+		this.yyPos = b.yyPos;
+		this.xxVel = b.xxVel;
+		this.yyVel = b.yyVel;
+		this.mass = b.mass;
+		this.imgFileName = b.imgFileName;
+	}
+}
