6e7303c1437681c61784f9b6aeb7676cb522083b
Willis Ng <wng11@stuy.edu>
2019-07-25 22:13:48 -0400

    Finished Unionfind

--- lab6/UnionFind.java
+++ lab6/UnionFind.java
@@ -1,35 +1,44 @@
-public class UnionFind {
+import java.util.NoSuchElementException;
+import java.util.HashSet;
 
-    // TODO - Add instance variables?
+public class UnionFind {
+    /** The parent array will keep track of the heads of each vertex */
+    int[] parent;
 
     /* Creates a UnionFind data structure holding n vertices. Initially, all
-       vertices are in disjoint sets. */
+       vertices are in disjoint sets, we will keep track of size by keeping the value
+       negative which will still allow the set to find the head. */
     public UnionFind(int n) {
-        // TODO
+        parent = new int[n];
+        for (int i = 0; i < n; i++) {
+            parent[i] = -1;
+        }
     }
 
     /* Throws an exception if v1 is not a valid index. */
-    private void validate(int vertex) {
-        // TODO
+    private void validate(int v1) {
+        if (v1 >= parent.length) {
+            throw new IllegalArgumentException("Out of Bounds");
+        }
     }
 
     /* Returns the size of the set v1 belongs to. */
     public int sizeOf(int v1) {
-        // TODO
-        return -1;
+        v1 = find(v1);
+        return parent(v1);
     }
 
     /* Returns the parent of v1. If v1 is the root of a tree, returns the
        negative size of the tree for which v1 is the root. */
     public int parent(int v1) {
-        // TODO
-        return -1;
+        return parent[v1];
     }
 
     /* Returns true if nodes v1 and v2 are connected. */
     public boolean connected(int v1, int v2) {
-        // TODO
-        return false;
+        v1 = find(v1);
+        v2 = find(v2);
+        return v1 == v2;
     }
 
     /* Connects two elements v1 and v2 together. v1 and v2 can be any valid 
@@ -38,14 +47,35 @@ public class UnionFind {
        vertex with itself or vertices that are already connected should not 
        change the sets but may alter the internal structure of the data. */
     public void union(int v1, int v2) {
-        // TODO
+        v1 = find(v1);
+        v2 = find(v2);
+        int s1 = parent[v1];
+        int s2 = parent[v2];
+        // Now that we have the two roots we compare size
+        if (s1 > s2) {
+            parent[v2] = v1;
+            parent[v1] += s2;
+        } else {
+            parent[v1] = v2;
+        }
     }
 
     /* Returns the root of the set V belongs to. Path-compression is employed
        allowing for fast search-time. */
     public int find(int vertex) {
-        // TODO
-        return -1;
+        HashSet<Integer> nodes = new HashSet<>();
+        while (true) {
+            if (parent(vertex) <= 0) {
+                break;
+            } else {
+                nodes.add(vertex);
+                vertex = parent(vertex);
+            }
+        }
+        for (int node : nodes) {
+            parent[node] = vertex;
+        }
+        return vertex;
     }
 
 }
