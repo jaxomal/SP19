141c98e2b3cc733369551b7450fc5ef80e122e1b
Willis Ng <wng11@stuy.edu>
2019-08-14 21:58:44 -0400

    Added comments to the code

--- proj2ab/bearmaps/ArrayHeapMinPQ.java
+++ proj2ab/bearmaps/ArrayHeapMinPQ.java
@@ -9,12 +9,19 @@ import java.util.HashMap;
  * another, we simply assign a priority value using the add or
  * changePriority functions.
  * @param <T> a generic type.
+ * @author willi
  */
 public class ArrayHeapMinPQ<T> implements ExtrinsicMinPQ<T> {
+    /** The heap, bts of the PQ. */
     private Entry[] pq;
+    /** The amount of things the PQ is holding at a given time. */
     private int size;
+    /** Stores the corresponding array position to an item, saves time. */
     private HashMap<T, Integer> map;
 
+    /**
+     * @param initCapacity the initial size of the array.
+     */
     @SuppressWarnings("unchecked")
     public ArrayHeapMinPQ(int initCapacity) {
         pq = (Entry[]) new ArrayHeapMinPQ<?>.Entry[initCapacity + 1];
@@ -25,26 +32,41 @@ public class ArrayHeapMinPQ<T> implements ExtrinsicMinPQ<T> {
     public ArrayHeapMinPQ() {
         this(1);
     }
+
     /** Adds an item with the given priority value. Throws an
-     * IllegalArgumentExceptionb if item is already present.
+     * IllegalArgumentException if item is already present.
      * You may assume that item is never null.
+     * @param item the item you want to add.
+     * @param priority the priority of the item.
      */
     public void add(T item, double priority) {
+        if (map.containsKey(item)) {
+            throw new IllegalArgumentException();
+        }
         if (size == pq.length - 1) {
             resize(2 * pq.length);
         }
         pq[++size] = new Entry(item, priority);
         swim(size);
     }
-    /* Returns true if the PQ contains the given item. */
+
+    /** Returns true if the PQ contains the given item.
+     * @param item the item you are checking for
+     */
     public boolean contains(T item) {
-        return find(item, 1) > 0;
+        return map.containsKey(item);
     }
-    /* Returns the minimum item. Throws NoSuchElementException if the PQ is empty. */
+
+    /** Returns the minimum item. Throws NoSuchElementException
+     * if the PQ is empty.
+     */
     public T getSmallest() {
         return pq[1].item;
     }
-    /* Removes and returns the minimum item. Throws NoSuchElementException if the PQ is empty. */
+
+    /** Removes and returns the minimum item. Throws
+     * NoSuchElementException if the PQ is empty.
+     */
     public T removeSmallest() {
         if (size == 0) {
             throw new NoSuchElementException();
@@ -59,16 +81,22 @@ public class ArrayHeapMinPQ<T> implements ExtrinsicMinPQ<T> {
         map.remove(min.item);
         return min.item;
     }
-    /* Returns the number of items in the PQ. */
+    
+    /** Returns the number of items in the PQ. */
     public int size() {
         return size;
     }
 
+    /** Returns the capacity of the underlying heap array */
     public int getCapacity() {
         return pq.length;
     }
-    /* Changes the priority of the given item. Throws NoSuchElementException if the item
-     * doesn't exist. */
+
+    /** Changes the priority of the given item.
+     * Throws NoSuchElementException if the item doesn't exist.
+     * @param item the thing you want to add.
+     * @param priority the lower the priority the closer to the front it will be.
+     */
     public void changePriority(T item, double priority) {
         if (!map.containsKey(item)) {
             throw new NoSuchElementException();
@@ -82,17 +110,33 @@ public class ArrayHeapMinPQ<T> implements ExtrinsicMinPQ<T> {
         }
     }
 
+    /**
+     * A private class that allows for the pair storage of an
+     * item and a priority in the Priority Queue.
+     */
     private class Entry {
+        /** An item of type T. */
         private T item;
+        /** The priority of the item. */
         private double priority;
 
-        public Entry(T item, double priority) {
+        /**
+         * @param item the thing you want to add.
+         * @param priority the lower the priority the closer to the
+         * front it will be.
+         */
+        Entry(T item, double priority) {
             this.item = item;
             this.priority = priority;
         }
     }
 
-    /** Helper Methods */
+    /**
+     * Given a capacity, the array will enlarge itself to hold capacity
+     * amount of items, therefore the capacity should be above the
+     * original size of the array.
+     * @param capacity the amount of stuff the array should hold.
+     */
     @SuppressWarnings("unchecked")
     private void resize(int capacity) {
         Entry[] temp = (Entry[]) new ArrayHeapMinPQ<?>.Entry[capacity];
@@ -102,29 +146,11 @@ public class ArrayHeapMinPQ<T> implements ExtrinsicMinPQ<T> {
         pq = temp;
     }
 
-    private int find(T item) {
-        return find(item, 1);
-    }
-
-    private int find(T item, int pos) {
-        if (pos >= pq.length || pq[pos] == null) {
-            return -1;
-        }
-        if (pq[pos].item.equals(item)) {
-            return pos;
-        }
-        int posLeft = find(item, pos * 2);
-        if (posLeft > 0) {
-            return posLeft;
-        }
-        int posRight = find(item, pos * 2 + 1);
-        if (posRight > 0) {
-            return posRight;
-        } else {
-            return -1;
-        }
-    }
-
+    /**
+     * Given the index of an entry, swim() will swim (move upwards)
+     * the entry to the right place.
+     * @param k the index of an entry.
+     */
     private void swim(int k) {
         while (k > 1 && greater(k / 2, k)) {
             swap(k, k / 2);
@@ -132,6 +158,11 @@ public class ArrayHeapMinPQ<T> implements ExtrinsicMinPQ<T> {
         }
     }
 
+    /**
+     * Given the index of an entry, sink() will sink (move downwards)
+     * the entry to the right place.
+     * @param k the index of an entry.
+     */
     private void sink(int k) {
         while (2 * k <= size) {
             int j = 2 * k;
@@ -146,6 +177,11 @@ public class ArrayHeapMinPQ<T> implements ExtrinsicMinPQ<T> {
         }
     }
 
+    /**
+     * Swaps the two entries given their index.
+     * @param j index of an entry.
+     * @param k index of another entry.
+     */
     private void swap(int j, int k) {
         Entry temp = pq[j];
         pq[j] = pq[k];
@@ -154,10 +190,16 @@ public class ArrayHeapMinPQ<T> implements ExtrinsicMinPQ<T> {
         map.put(temp.item, k);
     }
 
+    /**
+     * @param j index of an entry.
+     * @param k index of another entry.
+     * @return true if j > k, otherwise false.
+     */
     private boolean greater(int j, int k) {
         if (pq[j] == null) {
             return false;
-        } else if (pq[k] == null) {
+        }
+        if (pq[k] == null) {
             return true;
         }
         Entry pos0 = pq[j];
@@ -167,8 +209,5 @@ public class ArrayHeapMinPQ<T> implements ExtrinsicMinPQ<T> {
         } else {
             return false;
         }
-    }
-
-    public static void main(String[] args) {
     }
 }
